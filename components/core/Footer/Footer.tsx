1e0fbabf63a11dd817f812827780a5a5f965b9cf
AdaptCharm <jamie@jamie.no>
2020-12-04 07:50:51 +0100

    Add footer component

--- components/core/Footer/Footer.tsx
+++ components/core/Footer/Footer.tsx
@@ -6,37 +6,59 @@ const Footer: FC = () => {
   return (
     <footer className={f.footer}>
       <div className={f.overlay}></div>
-      <div className={f.inner}>
-        <div className={f.content}>
+      <div className="inner">
+        <div className={f.section}>
+          <h2 className="text-white">
+            Internships
+          </h2>
+          <p className="text-white">
+            Are you interested in working in a museum? Do you enjoy 
+            the fun and excitement of sharing the wonders of nature 
+            with people?
+          </p>
+          <p className="text-white">
+            Well you could be just the right person to enjoy an 
+            internship at the museum.
+          </p>
           <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className={f.link}
-            href="/contact"
-          >
-            Contact
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className={f.link}
+            className="button light"
             href="/about"
           >
-            About
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className={f.link}
-            href="/privacy-policy"
-          >
-            Privacy Policy
+            Apply Now
           </a>
         </div>
-        <a href="/" className="logo" aria-label="Logo">
-          <Logo size={8} />
-          Community Science Museum
-        </a>
+        <div className={f.section}>
+          <div className={f.nav}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`${f.link} text-white`}
+              href="/contact"
+            >
+              Contact
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`${f.link} text-white`}
+              href="/about"
+            >
+              About
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`${f.link} text-white`}
+              href="/privacy-policy"
+            >
+              Privacy Policy
+            </a>
+          </div>
+          <a href="/" className="logo" aria-label="Logo">
+            <Logo size={8} />
+            Community Science Museum
+          </a>
+        </div>
       </div>
     </footer>
   )
