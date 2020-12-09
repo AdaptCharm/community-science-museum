42af553ade89d9376d40606695b83b3cfd87e52e
AdaptCharm <jamie@jamie.no>
2020-12-09 09:12:35 +0100

    Minimize code

--- components/ui/Kids/Kids.tsx
+++ components/ui/Kids/Kids.tsx
@@ -7,26 +7,24 @@ const Kids: FC = () => {
         <h1 className="text-center">
           For <strong>Kids</strong>
         </h1>
-        <div className="flex flex-col flex-md-row gap-8 mb-8">
-          <div className="card">
-            <p>
+        <div className="grid grid-cols-md-2 gap-4">
+          <div className="flex flex-col shadow-small rounded-small bg-white p-6">
+            <p className="max-w-none">
               Are you a young person looking to learn more about science? 
               Come on down to our museum, there’s plenty to see and do.
             </p>
           </div>
-          <div className="card">
-            <p>
+
+          <div className="flex flex-col shadow-small rounded-small bg-white p-6">
+            <p className="max-w-none">
               We also have the Young Stars club which meets once a week on 
               a Saturday between 10:00 and 13:00 where you’ll get to explore 
               and experiment with our team of experts.
             </p>
           </div>
-        </div>
 
-
-        <div className="flex flex-col flex-md-row gap-8 mb-8">
-          <div className="card">
-            <p>
+          <div className="flex flex-col shadow-small rounded-small bg-white p-6">
+            <p className="max-w-none">
               You can learn about Newtonian physics from our bumper swing, 
               or why not travel back in time and meet our resident Woolly 
               Mammoth? Our exhibits are designed to be accessible for 
@@ -34,8 +32,9 @@ const Kids: FC = () => {
               explore.
             </p>
           </div>
-          <div className="card">
-            <p>
+
+          <div className="flex flex-col shadow-small rounded-small bg-white p-6">
+            <p className="max-w-none">
               During the school holidays we run special holiday clubs where 
               you can join other children your age to go on a journey of 
               discovery. Each holiday we pick a new theme to explore. To find 
--- components/ui/Teachers/Teachers.tsx
+++ components/ui/Teachers/Teachers.tsx
@@ -7,24 +7,26 @@ const Teachers: FC = () => {
         <h2 className="text-center">
           For <strong>Teachers</strong>
         </h2>
-        <div className="flex flex-col items-center gap-8 mb-8">
-          <div className="card">
-            <p>
-              As a community-driven museum, we want to work with schools 
-              to create places of learning and exploring. Our team are on 
-              hand to give your students guided tours of the museum, teach 
-              them in our learning laboratory, and provide great video 
-              presentations that will excite and inspire them.
-            </p>
-          </div>
-          <div className="card">
-            <p>
-              Please get in touch with us here to find out more about our 
-              facilities and to arrange a time to bring your students through 
-              for a visit.
-            </p>
-          </div>
-        </div>    
+        <div className="flex flex-col items-center">
+          <p>
+            As a community-driven museum, we want to work with schools 
+            to create places of learning and exploring. Our team are on 
+            hand to give your students guided tours of the museum, teach 
+            them in our learning laboratory, and provide great video 
+            presentations that will excite and inspire them.
+          </p>
+          <p className="mt-4">
+            Please get in touch with us below to find out more about our 
+            facilities and to arrange a time to bring your students through 
+            for a visit.
+          </p>
+          <a
+            className="button dark mt-4"
+            href="/contact"
+          >
+            Get in Touch
+          </a>
+        </div>
       </div>
     </section>
   )
